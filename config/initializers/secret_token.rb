3f0fe88c155dc4e38178d6d71651ac9664c8a498
Juan Pablo <jp.valdesgramer@gmail.com>
2013-05-08 14:13:15 -0400

    aplicacion rails hecha

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Quesos::Application.config.secret_token = 'a8de5912ed6f59464498621fa68cdaf730e7d1aabf376bf245be84d3bf4cda6cc8c75e56e70f3307485aef24dbdbfacdc8b11142ae5f063ba06a8acae2eb10ac'
