67a86a8ec880b638d648db32ab2cd8a53c31dc6a
Diego <dgcastil@uc.cl>
2013-05-18 11:54:03 -0400

    ultimo

--- app/models/processor.rb
+++ app/models/processor.rb
@@ -5,20 +5,91 @@ class Processor < ActiveRecord::Base
   include Reservas
   include Twit
   include Vtiger
+  include Stocks
   # attr_accessible :title, :body
   def start()
   		pedidos=load_orders()
   		pedidos.each do |pedido|
   		if pedido[:rut]== "34.242.924-1"do
-  			#sacar de bodega
- 	 		end
- 	 	pedido[:state]="despachado"
+  			
+           getStock(pedido[:sku]).each do |almacen|
+          if almacen[:tipo]=="despacho" 
+            
+            if almacen[:libre]>=pedido[:qty] 
+                despacharStock(almacen[:almacenId],pedido[:sku],pedido[:qty])
+                  pedido[:state]="despachado"
 
- 	 	direccion=get_address(pedido[:addressId])
- 	 	mensaje="Se han despachado #{pedido[:qty]} #{pedido[:unit]} del producto sku:#{pedido[:sku]}"
- 	 	pedido[:state] = "despachado"
- 	 	new_marker(direccion, mensaje, pedido[:orderDate])
-  		end
+                 direccion=get_address(pedido[:addressId])
+                 mensaje="Se han despachado #{pedido[:qty]} #{pedido[:unit]} del producto sku:#{pedido[:sku]}"
+                pedido[:state] = "despachado"
+                     new_marker(direccion, mensaje, pedido[:orderDate])
+
+                
+            else
+                getStock(pedido[:sku]).each do |almacen2|
+                   if almacen2[:tipo]=="Bodega Central de libre disposicion" or almacen2[:tipo]=="recepcion"
+                     moverStock(almacen2[:almacenId],almacen[:almacenId],pedido[:sku],almacen2[:libre])
+                   end
+
+                end
+                if almacen[:libre]>=pedido[:qty] 
+
+                despacharStock(almacen[:almacenId],pedido[:sku],pedido[:qty])
+                  pedido[:state]="despachado"
+
+                direccion=get_address(pedido[:addressId])
+                 mensaje="Se han despachado #{pedido[:qty]} #{pedido[:unit]} del producto sku:#{pedido[:sku]}"
+                pedido[:state] = "despachado"
+                 new_marker(direccion, mensaje, pedido[:orderDate])
+
+                end
+            end
+          end
+
+
+        end
+
+      else
+        if pedido[:rut]== "34.242.924-1"do
+        
+           getStock(pedido[:sku]).each do |almacen|
+          if almacen[:tipo]=="despacho" 
+            
+            if almacen[:libre]>=pedido[:qty] 
+                despacharStock(almacen[:almacenId],pedido[:sku],pedido[:qty])
+                  pedido[:state]="despachado"
+
+                 direccion=get_address(pedido[:addressId])
+                 mensaje="Se han despachado #{pedido[:qty]} #{pedido[:unit]} del producto sku:#{pedido[:sku]}"
+                pedido[:state] = "despachado"
+                     new_marker(direccion, mensaje, pedido[:orderDate])
+
+                
+            else
+                getStock(pedido[:sku]).each do |almacen2|
+                   if almacen2[:tipo]=="Bodega Central de libre disposicion" or almacen2[:tipo]=="recepcion"
+                     moverStock(almacen2[:almacenId],almacen[:almacenId],pedido[:sku],almacen2[:libre])
+                   end
+
+                end
+                if almacen[:libre]>=pedido[:qty] 
+
+                despacharStock(almacen[:almacenId],pedido[:sku],pedido[:qty])
+                  pedido[:state]="despachado"
+
+                direccion=get_address(pedido[:addressId])
+                 mensaje="Se han despachado #{pedido[:qty]} #{pedido[:unit]} del producto sku:#{pedido[:sku]}"
+                pedido[:state] = "despachado"
+                 new_marker(direccion, mensaje, pedido[:orderDate])
+
+                end
+            end
+          end
+
+
+        end
+ 	 		 end
+ 	   	end
 
 
   end 
