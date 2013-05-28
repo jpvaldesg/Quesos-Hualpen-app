3d7a59434e9270f051128ecf89e41b9be68290a8
Juan Pablo <jp.valdesgramer@gmail.com>
2013-05-28 15:22:18 -0400

    Agregado el ID para los pedidos

--- app/models/processor.rb
+++ app/models/processor.rb
@@ -8,89 +8,69 @@ class Processor < ActiveRecord::Base
   include Stocks
   # attr_accessible :title, :body
   def start()
-  		pedidos=load_orders()
-  		pedidos.each do |pedido|
-  		if pedido[:rut]== "34.242.924-1"do
-  			
-           getStock(pedido[:sku]).each do |almacen|
-          if almacen[:tipo]=="despacho" 
-            
+  	pedidos=load_orders()	    
+    pedidos.each do |pedido|
+    	if pedido[:rut]== "34.242.924-1" 
+        getStock(pedido[:sku]).each do |almacen|
+          if almacen[:tipo]=="despacho"   
             if almacen[:libre]>=pedido[:qty] 
-                despacharStock(almacen[:almacenId],pedido[:sku],pedido[:qty])
-                  pedido[:state]="despachado"
-
-                 direccion=get_address(pedido[:addressId])
-                 mensaje="Se han despachado #{pedido[:qty]} #{pedido[:unit]} del producto sku:#{pedido[:sku]}"
-                pedido[:state] = "despachado"
-                     new_marker(direccion, mensaje, pedido[:orderDate])
-
-                
-            else
-                getStock(pedido[:sku]).each do |almacen2|
-                   if almacen2[:tipo]=="Bodega Central de libre disposicion" or almacen2[:tipo]=="recepcion"
-                     moverStock(almacen2[:almacenId],almacen[:almacenId],pedido[:sku],almacen2[:libre])
-                   end
-
-                end
-                if almacen[:libre]>=pedido[:qty] 
-
-                despacharStock(almacen[:almacenId],pedido[:sku],pedido[:qty])
-                  pedido[:state]="despachado"
-
-                direccion=get_address(pedido[:addressId])
-                 mensaje="Se han despachado #{pedido[:qty]} #{pedido[:unit]} del producto sku:#{pedido[:sku]}"
-                pedido[:state] = "despachado"
-                 new_marker(direccion, mensaje, pedido[:orderDate])
-
-                end
+              despacharStock(almacen[:almacenId],pedido[:sku],pedido[:qty])
+              pedido[:state]="despachado"
+              direccion=get_address(pedido[:addressId])
+              mensaje="Se han despachado #{pedido[:qty]} #{pedido[:unit]} del producto sku:#{pedido[:sku]}"
+              pedido[:state] = "despachado"
+              new_marker(direccion, mensaje, pedido[:orderDate])
             end
+            else
+              getStock(pedido[:sku]).each do |almacen2|
+                if almacen2[:tipo]=="Bodega Central de libre disposicion" or almacen2[:tipo]=="recepcion"
+                  moverStock(almacen2[:almacenId],almacen[:almacenId],pedido[:sku],almacen2[:libre])
+                end
+              end
+             
+              if almacen[:libre]>=pedido[:qty] 
+                despacharStock(almacen[:almacenId],pedido[:sku],pedido[:qty])
+                pedido[:state]="despachado"
+                direccion=get_address(pedido[:addressId])
+                mensaje="Se han despachado #{pedido[:qty]} #{pedido[:unit]} del producto sku:#{pedido[:sku]}"
+                pedido[:state] = "despachado"
+                new_marker(direccion, mensaje, pedido[:orderDate]) 
+              end  
+            end 
           end
-
-
         end
-
+      end
       else
-        if pedido[:rut]== "34.242.924-1"do
-        
-           getStock(pedido[:sku]).each do |almacen|
-          if almacen[:tipo]=="despacho" 
-            
+        getStock(pedido[:sku]).each do |almacen|
+          if almacen[:tipo]=="despacho"   
             if almacen[:libre]>=pedido[:qty] 
-                despacharStock(almacen[:almacenId],pedido[:sku],pedido[:qty])
-                  pedido[:state]="despachado"
-
-                 direccion=get_address(pedido[:addressId])
-                 mensaje="Se han despachado #{pedido[:qty]} #{pedido[:unit]} del producto sku:#{pedido[:sku]}"
-                pedido[:state] = "despachado"
-                     new_marker(direccion, mensaje, pedido[:orderDate])
-
-                
-            else
-                getStock(pedido[:sku]).each do |almacen2|
-                   if almacen2[:tipo]=="Bodega Central de libre disposicion" or almacen2[:tipo]=="recepcion"
-                     moverStock(almacen2[:almacenId],almacen[:almacenId],pedido[:sku],almacen2[:libre])
-                   end
-
-                end
-                if almacen[:libre]>=pedido[:qty] 
-
-                despacharStock(almacen[:almacenId],pedido[:sku],pedido[:qty])
-                  pedido[:state]="despachado"
-
-                direccion=get_address(pedido[:addressId])
-                 mensaje="Se han despachado #{pedido[:qty]} #{pedido[:unit]} del producto sku:#{pedido[:sku]}"
-                pedido[:state] = "despachado"
-                 new_marker(direccion, mensaje, pedido[:orderDate])
-
-                end
+              despacharStock(almacen[:almacenId],pedido[:sku],pedido[:qty])
+              pedido[:state]="despachado"
+              direccion=get_address(pedido[:addressId])
+              mensaje="Se han despachado #{pedido[:qty]} #{pedido[:unit]} del producto sku:#{pedido[:sku]}"
+              pedido[:state] = "despachado"
+              new_marker(direccion, mensaje, pedido[:orderDate])
             end
+            else
+              getStock(pedido[:sku]).each do |almacen2|
+                if almacen2[:tipo]=="Bodega Central de libre disposicion" or almacen2[:tipo]=="recepcion"
+                  moverStock(almacen2[:almacenId],almacen[:almacenId],pedido[:sku],almacen2[:libre])
+                end
+              end
+             
+              if almacen[:libre]>=pedido[:qty] 
+                despacharStock(almacen[:almacenId],pedido[:sku],pedido[:qty])
+                pedido[:state]="despachado"
+                direccion=get_address(pedido[:addressId])
+                mensaje="Se han despachado #{pedido[:qty]} #{pedido[:unit]} del producto sku:#{pedido[:sku]}"
+                pedido[:state] = "despachado"
+                new_marker(direccion, mensaje, pedido[:orderDate]) 
+              end  
+            end 
           end
-
-
         end
- 	 		 end
- 	   	end
-
-
-  end 
+      end 
+    end
+  end
 end
+                
