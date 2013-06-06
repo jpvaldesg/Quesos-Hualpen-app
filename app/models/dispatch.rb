841c9ae586c7fc614c86d86a6e47c2ce2599d3b0
SH <sh@sh-VirtualBox.(none)>
2013-06-06 16:39:47 -0400

    cambio menor

--- app/models/dispatch.rb
+++ app/models/dispatch.rb
@@ -9,8 +9,8 @@ class Dispatch < ActiveRecord::Base
   def self.to_csv(options = {})
     CSV.generate(options) do |csv|
       csv << column_names
-      all.each do |product|
-        csv << product.attributes.values_at(*column_names)
+      all.each do |dispatch|
+        csv << dispatch.attributes.values_at(*column_names)
       end
     end
   end
